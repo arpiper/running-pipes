5132a2975fb2db5917c7889534ee0d59b6a2b618
Andrew Piper <pipera@uw.edu>
2018-09-17 15:38:01 -0700

    github logo/link. assets

--- frontendrun/src/components/Footer.vue
+++ frontendrun/src/components/Footer.vue
@@ -1,9 +1,15 @@
 <template>
   <footer class="app__footer">
     <div class="footer__left">
-      <p class="footer__left_text">
+      <span class="footer__left_text">
         &copy;2018 Andrew Piper
-      </p>
+      </span>
+      <span class="footer__left_logo">
+        <a href="https://github.com/arpiper/running-pipes" target="_blank" noopener>
+        <img class="footer__left_logo_github" :src="githubLogo">
+          Github.com/arpiper
+        </a>
+      </span>
     </div>
     <div class="footer__right">
       <img class="footer__right_logo_strava" :src="pwrdByStrava" alt="Powered By Strava">
@@ -17,6 +23,7 @@ export default {
   data () {
     return {
       pwrdByStrava: require('@/assets/api_logo_pwrdBy_strava_horiz_light.png'),
+      githubLogo: require('@/assets/GitHub-Mark-64px.png'),
     }
   },
 }
@@ -28,18 +35,27 @@ export default {
   align-items: center;
   justify-content: space-around;
   height: 50px;
+  font-size: 12px;
 }
 .footer__left {
   display: flex;
   align-items: center;
+  flex-direction: column;
+  height: 75%;
+}
+.footer__left_text {
+  margin: 0;
+}
+.footer__left_logo {
+  height: 14px;
+}
+.footer__left_logo_github {
+  height: 100%;
 }
 .footer__right {
   display: flex;
   align-items: center;
   height: 75%;
-}
-.footer__left_text {
-  margin: 0;
 }
 .footer__right_logo_strava {
   height: 100%;
