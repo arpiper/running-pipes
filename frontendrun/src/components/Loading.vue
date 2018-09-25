7615fc508c4120a21a545c5c1102124c354576f4
Andrew Piper <pipera@uw.edu>
2018-09-24 17:01:37 -0700

    moved loading animation to separate componnent

---
+++ frontendrun/src/components/Loading.vue
@@ -0,0 +1,22 @@
+<template>
+  <div v-if="loading">
+    <div class="loading__bar_container">
+      <span  class="loading__bar">
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'loading-cmp',
+  props: {
+    loading: Boolean,
+  },
+}
+</script>
+
+<style>
+
+</style>
